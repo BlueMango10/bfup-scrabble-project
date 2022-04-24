9dcf550fe6c165de445e17c231f789f9974d7c19
Magnus Dalsgaard Larsen <daml@itu.dk>
2022-04-24 16:11:59 +0200

    Ready to make the AI part
    Co-authored-by: Gustavholm2 <Gustavholm2@users.noreply.github.com>

--- ScrabbleBot/Scrabble.fs
+++ ScrabbleBot/Scrabble.fs
@@ -47,97 +47,132 @@ module State =
         playerNumber  : uint32
         hand          : MultiSet.MultiSet<uint32>
         pieces        : Map<coord, (char * int)>
+        totalPlayers  : uint32
+        turn          : uint32
     }
 
-    let mkState b d pn h p = {board = b; dict = d;  playerNumber = pn; hand = h; pieces = p; }
+    let mkState b d pn h p tp t =
+        {board = b; dict = d;  playerNumber = pn; hand = h; pieces = p; totalPlayers = tp; turn = t;}
 
     let board st         = st.board
     let dict st          = st.dict
     let playerNumber st  = st.playerNumber
     let hand st          = st.hand
     let pieces st        = st.pieces
+    let totalPlayers st  = st.totalPlayers
+    let turn st          = st.turn
 
     let withBoard b st         = {st with board = b}
     let withDict d st          = {st with dict = d}
     let withPlayerNumber pn st = {st with playerNumber = pn}
     let withHand h st          = {st with hand = h}
     let withPieces p st        = {st with pieces = p}
+    let withTotalPlayers tp st = {st with totalPlayers = tp}
+    let withTurn t st          = {st with turn = t}
 
 module Scrabble =
     open System.Threading
 
     let playGame cstream pieces (st : State.state) =
 
+        let nextTurn t st =
+            st |> State.withTurn ((t % (State.totalPlayers st)) + 1u) // IDs start from 1
+
+        let placePieces (co: coord, (_,(piece: char * int))) st =
+            st |> State.withPieces (Map.add co piece (State.pieces st))
+
+        let removeFromHand (_,((id: uint32),_)) st =
+            st |> State.withHand (MultiSet.removeSingle id (State.hand st))
+
+        let addToHand (id,n) st =
+            st |> State.withHand (MultiSet.add id n (State.hand st))
+
+
+        let doMove hasTurn st =
+            match hasTurn with
+            | false -> None
+            | true  ->
+                Print.printHand pieces (State.hand st)
+
+                // remove the force print when you move on from manual input (or when you have learnt the format)
+                forcePrint "Input move (format '(<x-coordinate> <y-coordinate> <piece id><character><point-value> )*', note the absence of space between the last inputs)\n\n"
+                // This is the part where the move is defined:
+                (*
+                    This part will be replaced with our move-finding algorithm.
+                    The result will be stored in the `move` variable.
+                *)
+                let input =  System.Console.ReadLine() // Human input
+                let move = RegEx.parseMove input // Input parsed into an actual move
+
+                debugPrint (sprintf "Player %d -> Server:\n%A\n" (State.playerNumber st) move) // keep the debug lines. They are useful.
+                // Send move to server. This could also be other types of move such as SMChange.
+                send cstream (SMPlay move)
+
+                Some move
+
+
         let rec aux (st : State.state) =
-            Print.printHand pieces (State.hand st)
 
-            // remove the force print when you move on from manual input (or when you have learnt the format)
-            forcePrint "Input move (format '(<x-coordinate> <y-coordinate> <piece id><character><point-value> )*', note the absence of space between the last inputs)\n\n"
-            // This is the part where the move is defined:
-            (*
-                This part will be replaced with our move-finding algorithm.
-                The result will be stored in the `move` variable.
-            *)
-            let input =  System.Console.ReadLine() // Human input
-            let move = RegEx.parseMove input // Input parsed into an actual move
+            let hasTurn =
+                (State.turn st) = (State.playerNumber st)
 
-            debugPrint (sprintf "Player %d -> Server:\n%A\n" (State.playerNumber st) move) // keep the debug lines. They are useful.
-            // Send move to server. This could also be other types of move such as SMChange.
-            send cstream (SMPlay move)
+            let move = doMove hasTurn st
+            move |> ignore
 
             // Recieve message from server and bind it to `msg`.
+            debugPrint (sprintf "====> Listening for msg, turn is %d\n" (State.turn st))
             let msg = recv cstream
-            debugPrint (sprintf "Player %d <- Server:\n%A\n" (State.playerNumber st) move) // keep the debug lines. They are useful.
+            debugPrint (sprintf "Player %d <- Server:\n%A\n" (State.playerNumber st) msg) // keep the debug lines. They are useful.
+
 
             // Handle the response from the server.
             match msg with
             | RCM (CMPlaySuccess(ms, points, newPieces)) ->
+                debugPrint "====> PLAY SUCCESS\n"
                 (* Successful play by you. Update your state (remove old tiles, add the new ones, change turn, etc) *)
                 // Place pieces on board
                 // Remove pieces from hand
                 // Add new pieces to hand
-
-                let placePieces = fun (p: Map<coord, char * int>) (move: coord * (uint32 * (char * int))) ->
-                    p // remove when it works'
-                
-                let removeFromHand = fun (h: MultiSet.MultiSet<uint32>) (_,((id: uint32),_)) ->
-                    MultiSet.removeSingle id h
-                
-                let performMove = fun st ->
+                let performMove st =
                     List.fold (fun s m -> s
-                                       |> State.withPieces (placePieces (State.pieces s) m) // place pieces
-                                       |> State.withHand (removeFromHand (State.hand s) m)  // remove pieces from hand
+                                       |> placePieces m    // place pieces
+                                       |> removeFromHand m // remove pieces from hand
                     ) st ms
-                
-                let addToHand = fun (h: MultiSet.MultiSet<uint32>) (id,n) ->
-                    MultiSet.add id n h
-                let addNewTilesToHand = fun st ->
+                let addNewTilesToHand st =
                     List.fold (fun s np -> s
-                                        |> State.withHand (addToHand (State.hand s) np) // add piece(s) to hand
+                                        |> addToHand np // add piece(s) to hand
                     ) st newPieces
-
+                let updateTurn st = st
+                                    |> nextTurn (State.playerNumber st) // We know we made this move
                 // Combine to update state
-                let st' = st |> performMove |> addNewTilesToHand
+                let st' = st |> performMove |> addNewTilesToHand |> updateTurn
                 aux st'
+
+
             | RCM (CMPlayed (pid, ms, points)) ->
+                debugPrint "====> PLAYED\n"
                 (* Successful play by other player. Update your state *)
-                // Place tiles on board (based on `ms`)
-                (*
-                performMove = fun ->
-                    fold (* on each tile *) (
-                        place tile on board
-                    )
-                *)
-                (*
-                let st' = st |> performMove
-                *)
-                let st' = st // This state needs to be updated
+                // Place tiles on board
+                let performMove st =
+                    List.fold (fun s m -> s
+                                       |> placePieces m
+                    ) st ms
+                let updateTurn st = st
+                                    |> nextTurn pid
+                let st' = st |> performMove |> updateTurn
                 aux st'
+
+
             | RCM (CMPlayFailed (pid, ms)) ->
+                debugPrint "====> PLAY FAILED\n"
                 (* Failed play. Update your state *)
-                // Don't update
-                let st' = st // This state needs to be updated
+                // Only update turn
+                let updateTurn st = st
+                                    |> nextTurn pid
+                let st' = st |> updateTurn
                 aux st'
+
+
             | RCM (CMGameOver _) -> ()
             | RCM a -> failwith (sprintf "not implmented: %A" a)
             | RGPE err -> printfn "Gameplay Error:\n%A" err; aux st
@@ -169,5 +204,5 @@ module Scrabble =
                   
         let handSet = List.fold (fun acc (x, k) -> MultiSet.add x k acc) MultiSet.empty hand
 
-        fun () -> playGame cstream tiles (State.mkState board dict playerNumber handSet Map.empty)
+        fun () -> playGame cstream tiles (State.mkState board dict playerNumber handSet Map.empty numPlayers playerTurn)
         
